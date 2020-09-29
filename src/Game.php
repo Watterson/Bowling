88b0c378888959177963adc216bac611cbb6c8be
Rory Watterson <rwatterson96@hotmail.co.uk>
2020-09-29 12:59:43 +0100

    Added test & func for rolling one spare

--- src/Game.php
+++ src/Game.php
@@ -3,20 +3,46 @@ namespace Bowling;
 
 class Game
 {
-    protected $score;
+    protected $rolls = array(21);
+    protected $currentRoll = 0;
 
     /**
      * Game constructor.
-     * @param int $score
+     *
      */
-    public function __construct(int $score)
+    public function __construct()
     {
-        $this->score = $score;
+
     }
 
     public function roll($pins)
     {
-        $this->score += $pins;
-        return $this->score;
+        $this->rolls[$this->currentRoll++] = $pins;
+
+    }
+
+    public function score()
+    {
+        $score = 0;
+        $firstInFrame = 0;
+        for($frame = 0; $frame < 10; $frame++){
+            if($this->isSpare($firstInFrame)){
+                $score += 10 + $this->rolls[$firstInFrame+2];
+                $firstInFrame += 2;
+            }else{
+                $score += $this->rolls[$firstInFrame] + $this->rolls[$firstInFrame+1];
+                $firstInFrame += 2;
+            }
+        }
+        return $score;
+    }
+
+    public function isSpare($firstInFrame)
+    {
+        if($this->rolls[$firstInFrame] + $this->rolls[$firstInFrame+1] == 10){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -4,38 +4,50 @@ namespace Bowling\tests;
 use Bowling\Game;
 use PHPUnit\Framework\TestCase;
 
-Class GameTest extends TestCase
+Class ng GameTest extends TestCase
 {
     /**
      * @var Game  instance of Game class
-     * @var Score int
      */
 
     private $game;
-    private $score;
 
     public function setUp() :void
     {
-        $this->game = new Game(0);
+        $this->game = new Game();
     }
-    
+
     public function rollMany($n, $pins)
     {
-        for($i = 0; $i<$n; $i++){
-            $this->score = $this->game->roll($pins);
-        }
+        for($i = 0; $i < $n; $i++) $this->game->roll($pins);
+    }
+
+    public function rollSpare()
+    {
+        $this->game->roll(5);
+        $this->game->roll(5);
     }
 
     public function testRollGutterGame()
     {
         $this->rollMany(20,0);
-        $this->assertEquals(0, $this->score);
+        $this->assertEquals(0, $this->game->score());
     }
 
     public function testRollAllOnes()
     {
         $this->rollMany(20,1);
-        $this->assertEquals(20, $this->score);
+        $this->assertEquals(20, $this->game->score());
     }
+
+    public function testRollOneSpare()
+    {
+        $this->rollSpare();
+        $this->game->roll(3);
+        $this->rollMany(17, 0);
+        $this->assertEquals(16, $this->game->score());
+
+    }
+
 
 }
